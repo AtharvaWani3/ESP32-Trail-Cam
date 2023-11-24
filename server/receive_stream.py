3fda7b93d2fec6e4229f1c24589904f92375b6b6
AtharvaWani3 <aawani@asu.edu>
2023-11-24 14:52:47 -0700

    commit

--- server/receive_stream.py
+++ server/receive_stream.py
@@ -11,8 +11,7 @@ from pymongo import MongoClient
 import gridfs
 
 # Connect to MongoDB
-client = MongoClient("mongodb://54.163.61.80:27017")
-db = client["your_database_name"]  # Change 'your_database_name' to your actual database name
+client = MongoClient("mongodb://54.163.61.80:27017/ESP32_stream")
 fs = gridfs.GridFS(db)
 
 def is_valid_image(image_bytes):
